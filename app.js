7582719cd39f70f36c8dea06d693ac34caccdf80
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-08-01 13:56:18 +0200

    first html page via node

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const http = require("http");
+const { readFileSync } = require("fs");
+
+//get all files
+const homePage = readFileSync("./index.html");
 
 const server = http
   .createServer((req, res) => {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>home page </h1>");
-    res.end();
+    const url = req.url;
+    if (url === "/") {
+      res.writeHead(200, { "content-type": "text/html" });
+      res.write(homePage);
+      res.end();
+    } else if (url === "/about") {
+      res.writeHead(200, { "content-type": "text/html" });
+      res.write("<h1>about page </h1>");
+      res.end();
+    } else {
+      res.writeHead(404, { "content-type": "text/html" });
+      res.write("page not found");
+      res.end();
+    }
   })
   .listen(5000);
