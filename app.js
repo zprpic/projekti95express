b704eff10ba22553e2c87eaf4378b97fdb602e84
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-08-01 14:25:25 +0200

    nav-bar page with css and svgs and links with node.js

--- app.js
+++ app.js
@@ -2,20 +2,41 @@ const http = require("http");
 const { readFileSync } = require("fs");
 
 //get all files
-const homePage = readFileSync("./index.html");
+const homePage = readFileSync("./navbar-app/index.html");
+const homeStyles = readFileSync("./navbar-app/styles.css");
+const homeImage = readFileSync("./navbar-app/logo.svg");
+const homeLogic = readFileSync("./navbar-app/browser-app.js");
 
 const server = http
   .createServer((req, res) => {
     const url = req.url;
-    if (url === "/") {
+    console.log(url);
+    if (url === "/" || url === "/index.html") {
       res.writeHead(200, { "content-type": "text/html" });
       res.write(homePage);
       res.end();
-    } else if (url === "/about") {
+    } //style
+    else if (url === "/styles.css") {
+      res.writeHead(200, { "content-type": "text/css" });
+      res.write(homeStyles);
+      res.end();
+    } //logo
+    else if (url === "/logo.svg") {
+      res.writeHead(200, { "content-type": "image/svg+xml" });
+      res.write(homeImage);
+      res.end();
+    } //logic
+    else if (url === "/browser-app.js") {
+      res.writeHead(200, { "content-type": "text/javascript" });
+      res.write(homeLogic);
+      res.end();
+    } // about page
+    else if (url === "/about.html") {
       res.writeHead(200, { "content-type": "text/html" });
       res.write("<h1>about page </h1>");
       res.end();
-    } else {
+    } // 404 page
+    else {
       res.writeHead(404, { "content-type": "text/html" });
       res.write("page not found");
       res.end();
