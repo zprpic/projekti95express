f6a59f185d0b82b69f024954510a452fd2724bda
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-08-02 11:47:57 +0200

    query strings

--- app.js
+++ app.js
@@ -32,6 +32,25 @@ app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
   res.send("hello world");
 });
 
+app.get("/api/v1/query", (req, res) => {
+  //console.log(req.query);
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search);
+    });
+  }
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit));
+  }
+  if (sortedProducts.length < 1) {
+    //res.status(200).send("no products matched");
+    return res.status(200).json({ success: true, data: [] });
+  }
+  res.status(200).json(sortedProducts);
+});
+
 app.listen(5000, () => {
   console.log("Server listening on port 5000");
 });
