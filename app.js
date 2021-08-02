7a6447ae0ad53521339db3ed2036fa92577a6f5a
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-08-02 09:24:56 +0200

    basic express example

--- app.js
+++ app.js
@@ -1,45 +1,26 @@
-const http = require("http");
-const { readFileSync } = require("fs");
+const express = require("express");
+const app = express();
 
-//get all files
-const homePage = readFileSync("./navbar-app/index.html");
-const homeStyles = readFileSync("./navbar-app/styles.css");
-const homeImage = readFileSync("./navbar-app/logo.svg");
-const homeLogic = readFileSync("./navbar-app/browser-app.js");
+app.get("/", (req, res) => {
+  res.status(200).send("Home Page");
+});
 
-const server = http
-  .createServer((req, res) => {
-    const url = req.url;
-    console.log(url);
-    if (url === "/" || url === "/index.html") {
-      res.writeHead(200, { "content-type": "text/html" });
-      res.write(homePage);
-      res.end();
-    } //style
-    else if (url === "/styles.css") {
-      res.writeHead(200, { "content-type": "text/css" });
-      res.write(homeStyles);
-      res.end();
-    } //logo
-    else if (url === "/logo.svg") {
-      res.writeHead(200, { "content-type": "image/svg+xml" });
-      res.write(homeImage);
-      res.end();
-    } //logic
-    else if (url === "/browser-app.js") {
-      res.writeHead(200, { "content-type": "text/javascript" });
-      res.write(homeLogic);
-      res.end();
-    } // about page
-    else if (url === "/about.html") {
-      res.writeHead(200, { "content-type": "text/html" });
-      res.write("<h1>about page </h1>");
-      res.end();
-    } // 404 page
-    else {
-      res.writeHead(404, { "content-type": "text/html" });
-      res.write("page not found");
-      res.end();
-    }
-  })
-  .listen(5000);
+app.get("/about", (req, res) => {
+  res.status(200).send("About page");
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>404</h1>");
+});
+
+app.listen(5000, (req, res) => {
+  console.log("Server is listening on port: 5000");
+});
+
+//app.get
+//app.post
+//app.put
+//app.delete
+//app.all
+//app.use
+//app.listen
