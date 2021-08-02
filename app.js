5defee5657737e8d7a9a8e67185568927f1819f6
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-08-02 13:59:35 +0200

    authorization via query object props

--- app.js
+++ app.js
@@ -3,13 +3,27 @@ const app = express();
 
 // req => middleware => res
 const logger = require("./logger");
+const authorize = require("./authorize");
 
-app.get("/", logger, (req, res) => {
+app.use([authorize, logger]);
+//app.use('/api', logger)
+//applies to /api/...EVERYTHING.../...EVERYTHING...
+
+app.get("/", (req, res) => {
   res.send("home page");
 });
 
-app.get("/about", logger, (req, res) => {
+app.get("/about", (req, res) => {
   res.send("about page");
+  console.log(req.user);
+});
+
+app.get("/api/products", (req, res) => {
+  res.send("Products");
+});
+
+app.get("/api/items", (req, res) => {
+  res.send("Items");
 });
 
 app.listen(5000, (req, res) => {
