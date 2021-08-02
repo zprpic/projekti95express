f4a3af03d58f2f7858e6219301fc313651a47092
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-08-02 09:36:26 +0200

    static resources added in /public

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).send("Home Page");
-});
+//setup static and middleware
+app.use(express.static("./public"));
 
-app.get("/about", (req, res) => {
-  res.status(200).send("About page");
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
 });
 
 app.all("*", (req, res) => {
-  res.status(404).send("<h1>404</h1>");
+  res.status(404).send("<h1>404 FUCK OFF</h1>");
 });
 
-app.listen(5000, (req, res) => {
+app.listen(5000, () => {
   console.log("Server is listening on port: 5000");
 });
 
