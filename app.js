dbcc364d84a2beb4020d22e90f39745ddb6d730c
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-08-03 12:50:03 +0200

    POST method with HTML form and with axios

--- app.js
+++ app.js
@@ -4,11 +4,23 @@ const { people } = require("./data");
 
 // static assets
 app.use(express.static("./methods-public"));
-// parse form data
+// parse html form data
 app.use(express.urlencoded({ extended: false }));
+//parse json from axios
+app.use(express.json());
 
 app.get("/api/people", (req, res) => {
   res.status(200).json({ success: true, data: people });
+});
+
+app.post("/api/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    res
+      .status(400)
+      .json({ success: false, message: "Please provide name value" });
+  }
+  res.status(201).json({ success: true, person: name });
 });
 
 app.post("/login", (req, res) => {
