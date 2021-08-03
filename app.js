1d012532356071727d85009d8d314e7489687ef4
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-08-03 13:19:45 +0200

    PUT method added

--- app.js
+++ app.js
@@ -42,6 +42,26 @@ app.post("/login", (req, res) => {
   }
 });
 
+app.put("/api/people/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  const person = people.find((person) => person.id === Number(id));
+
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, message: `no person found with ${id} found` });
+  }
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name;
+    }
+    return person;
+  });
+  res.status(200).json({ success: true, data: newPeople });
+});
+
 app.listen(5000, (req, res) => {
   console.log("Server listening on port 5000");
 });
